76335cb6bce23e20d831bdb5bf0aa5c762ad2177
NuriMetin <nurimetin98@gmail.com>
2019-11-05 10:17:09 +0400

    Initial

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.backgroundColor = "white";
+      document.getElementById("navbar").style.boxShadow= "0 15px 15px 0 rgba(0, 0, 0, 0.19)";
+    } else {
+      document.getElementById("navbar").style.background = "transparent";
+      document.getElementById("navbar").style.boxShadow= "none";
+    }
+  }
