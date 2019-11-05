41b25327bfc69a68a779eb0a4240adc3f8582f35
NuriMetin <nurimetin98@gmail.com>
2019-11-05 21:56:45 +0400

    fff

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      document.getElementById("navbar").style.backgroundColor = "white";
-      document.getElementById("navbar").style.boxShadow= "0 15px 15px 0 rgba(0, 0, 0, 0.19)";
+        document.getElementById("navbar").style.backgroundColor = "white";
+        document.getElementById("navbar").style.boxShadow = "0 15px 15px 0 rgba(0, 0, 0, 0.19)";
     } else {
-      document.getElementById("navbar").style.background = "transparent";
-      document.getElementById("navbar").style.boxShadow= "none";
+        document.getElementById("navbar").style.background = "transparent";
+        document.getElementById("navbar").style.boxShadow = "none";
     }
-  }
+}
+
+$(document).ready(function () {
+    $("#flip").click(function () {
+        $("#panel").slideToggle("fast");
+    });
+});
