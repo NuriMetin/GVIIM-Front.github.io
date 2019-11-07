0a1f88324d05e95aa613b20a1e8b8d3ced59a6b6
NuriMetin <nurimetin98@gmail.com>
2019-11-07 22:05:39 +0400

    sfkdh

--- js/index.js
+++ js/index.js
@@ -14,9 +14,10 @@ function scrollFunction() {
 
 $(document).ready(function () {
     $('.navbar-light .dmenu').hover(function () {
-        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
-    }, function () {
-        $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
     });
-});
 
+    
