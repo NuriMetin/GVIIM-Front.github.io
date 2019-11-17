7a12507a9ae63efa684798dd5062d43c52e267fb
NuriMetin <nurimetin98@gmail.com>
2019-11-17 21:08:05 +0400

    dfjdvnkdn

--- js/index.js
+++ js/index.js
@@ -20,13 +20,28 @@ $(document).ready(function () {
     });
 });
 
+
+//let array = ['.salam', '#qaqa', 'balam'];
+
+/**
+ * gets array of css selectors and applies display parameter to them
+ * @param {array} array 
+ * @param {string} display 
+ */
+function displayParam( array, display ){
+    !Array.isArray(array) && 
+        (document.querySelector(array).style.display = display) || 
+            array.forEach( (elem)=>{ document.querySelector(elem).style.display = display } );
+}
+
 $(document).ready(function () {
     if ($(window).width() <= 991) {
         $("#search").click(function () {
-            $("#gviim_element").css("display", "none");
-            $("#media_element").css("display", "none");
-            $("#news_element").css("display", "none");
-            $("#aztu_element").css("display", "none");
+            displayParam(['#gviim_element','#media_element','#news_element','#aztu_element'], 'none');
+            // $("#gviim_element").css("display", "none");
+            // $("#media_element").css("display", "none");
+            // $("#news_element").css("display", "none");
+            // $("#aztu_element").css("display", "none");
             $("#search").css("display", "block");
             $("#search").css("padding", "0px");
             $("#search_element").slideToggle("fast");
